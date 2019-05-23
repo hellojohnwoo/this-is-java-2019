9521ff8d9ad44070e3f12950e8e773d9b3d6872a
hellojohnwoo <hellojohnwoo@gmail.com>
2019-05-23 23:51:37 +0900

    0523 MethodReference java-study

---
+++ src/chap14lamda/sec06/O1/Calculator.java
@@ -0,0 +1,15 @@
+package chap14lamda.sec06.O1;
+
+public class Calculator {
+
+    public static int staticMethod(int x, int y) {
+        return x + y;
+    }
+
+
+    public        int instanceMethod(int x, int y) {
+        return x + y;
+    }
+
+
+}
---
+++ src/chap14lamda/sec06/O1/MethodReferencesEx.java
@@ -0,0 +1,23 @@
+package chap14lamda.sec06.O1;
+
+import java.util.function.IntBinaryOperator;
+
+public class MethodReferencesEx {
+    public static void main(String[] args) {
+        IntBinaryOperator operator;
+
+        operator = (x, y) -> Calculator.staticMethod(x, y);
+        System.out.println("result1 : " + operator.applyAsInt(1,2));
+
+        operator = Calculator :: staticMethod;
+        System.out.println("result2 : " + operator.applyAsInt(3,4));
+
+        Calculator obj = new Calculator();
+        operator = (x, y) -> obj.instanceMethod(x, y);
+        System.out.println("result3 : " + operator.applyAsInt(5,6));
+
+        operator = obj :: instanceMethod;
+        System.out.println("result4 : " + operator.applyAsInt(7,8));
+
+    }
+}
